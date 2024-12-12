f3f5a27b0e2839664fa2f8a2e61cea1e03b69a46
kaua_amelo <kaua.amelo2020@gmail.com>
2024-12-12 01:17:13 -0300

    feat: finalizar o footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,22 @@
 import style from '../footer/footer.module.scss'
+import logoFacebook from '../../assets/logoFacebook.png'
+import logoLinkedin from '../../assets/logoLinkedin.png'
+import logoInstagram from '../../assets/logoInstagram.png'
 
 export default function Footer(){
     return (
         <footer className={style.footer}>
-            <h2>Eu sou o footer</h2>
+            <section className={style.contatosFooter}>
+                <p>4002-8922</p>
+                <nav>
+                    <a href=""><img src={logoFacebook} alt="" /></a>
+                    <a href=""><img src={logoLinkedin} alt="" /></a>
+                    <a href=""><img src={logoInstagram} alt="" /></a>
+                </nav>
+            </section>
+            <section className={style.copyright}>
+                <p>Layout desenvolvido por Kauã Alves para projeto Vai Na Web para fins educativos - 2024</p>
+            </section>
         </footer>    
     )
 }
